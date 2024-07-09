f68e60f864cdcd925b74eaa6fc4080e3df07c617
Cris Vilaça <cristielemvilaca@gmail.com>
2024-07-08 22:48:29 -0400

    teste falhando

--- cypress/e2e/busca.cy.js
+++ cypress/e2e/busca.cy.js
@@ -12,7 +12,7 @@ describe('US-001 : funcionalidade: Busca de filmes', () => {
     it('Deve buscar filmes com sucesso', () => {
         cy.get('#search-input').type('Matrix')
         cy.get('#search-button').click()
-        cy.get('#results-section').should('contain', 'Matrix')
+        cy.get('#results-section').should('contain', 'Matrixxxxx')
     });
 
     it('Deve buscar filme inexistente', () => {
